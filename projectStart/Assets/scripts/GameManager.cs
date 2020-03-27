a48c1f9b89544264038a6761708d7a0becb7e5f5
unknown <nata2@uw.edu>
2020-03-27 15:57:15 -0700

    gamemanager and monster controller

---
+++ projectStart/Assets/scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    //this script will have all information stored about the game and game state
+    private static GameManager instance; //Singelton pattern
+    private string _currentLevel = string.Empty;
+    private readonly string[] LEVELS = { "ninjaStars", "breakObjects", "fightMonsters", "final" };
+    private int attackWave = -1; //which attackWave is currently active
+    public static GameManager GetInstance()
+    {
+        return instance;
+    }
+
+    public int GetAttackWave()
+    {
+        return attackWave;
+    }
+    void Awake()
+    {
+        if(instance == null)
+        {
+            
+            instance = this;
+            Debug.Log("Instance is created");
+        }
+        else
+        {
+            Debug.LogError("there are multiple game managers");
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        _currentLevel = LEVELS[0];
+        Debug.Log(_currentLevel);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ projectStart/Assets/scripts/MonsterController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MonsterController : MonoBehaviour
+{
+    public float speed = 4;
+    public int wave = 1;
+    NavMeshAgent agent;
+    GameObject player;
+    GameManager manager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        manager = GameManager.GetInstance();
+        if(agent == null || manager == null)
+        {
+            Debug.Log("something is wrong");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(manager.GetAttackWave() == wave)
+        {
+            //follow the Player!!
+        }
+        
+    }
+}
