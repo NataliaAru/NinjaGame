cbe0d2ba394f9196ba2e4b982c02a301c3e68780
unknown <nata2@uw.edu>
2020-03-29 16:19:23 -0700

    monster follows player

--- projectStart/Assets/scripts/MonsterController.cs
+++ projectStart/Assets/scripts/MonsterController.cs
@@ -7,28 +7,43 @@ public class MonsterController : MonoBehaviour
 {
     public float speed = 4;
     public int wave = 1;
-    NavMeshAgent agent;
-    GameObject player;
+    public float range = 2;
+    public Camera cam;
+    public NavMeshAgent agent;
+    public GameObject player;
     GameManager manager;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
         manager = GameManager.GetInstance();
         if(agent == null || manager == null)
         {
             Debug.Log("something is wrong");
         }
+
     }
 
     // Update is called once per frame
     void Update()
-    {
-        if(manager.GetAttackWave() == wave)
+    { 
+        if(manager.GetAttackWave() != null)
         {
+            if(Vector3.Distance(transform.position, player.transform.position) < range)
+            {
+                Debug.Log("stopped");
+                agent.SetDestination(transform.position);
+            }
+            else
+            {
+                Debug.Log("chasing");
+                agent.SetDestination(player.transform.position);
+            }
+                            
             //follow the Player!!
         }
+        
         
     }
 }
