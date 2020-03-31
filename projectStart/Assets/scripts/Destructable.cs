e4b9de916f61b7f40c0df4bf80630c1e2740bb34
CoreyS <coreyseaman@gmail.com>
2020-03-31 15:16:37 -0700

    Early Destruction

---
+++ projectStart/Assets/scripts/Destructable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructable : MonoBehaviour
+{
+    public GameObject shatteredversion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("sphere"))
+            ShatterObject();
+    }
+
+    void ShatterObject()
+    {
+        Instantiate(shatteredversion, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
